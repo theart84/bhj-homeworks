da26e2213321f1490caa61343039084a4e29b8a8
theart84 <artshadow74@gmail.com>
2020-12-12 18:40:05 +0500

    Task 1, part 1.2 completed

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -13,15 +13,56 @@ function showTooltipMessage(element) {
     return;
   }
   if (tooltips.length) {
-    tooltips.forEach((item) => item.remove());
+    tooltips.forEach((tooltip) => tooltip.remove());
   }
-  const { left, bottom } = link.getBoundingClientRect();
-  const template = createTooltipTemplate(left, bottom, element.title);
+  const currentPosition = link.dataset.position;
+  const proportionsTooltip = getProportionsTooltip(link);
+  const position = calcPosition(currentPosition, proportionsTooltip, link.getBoundingClientRect());
+  const template = createTooltipTemplate(position, element.title);
   link.insertAdjacentHTML('afterend', template);
 }
 
-function createTooltipTemplate(left, bottom, title) {
+function calcPosition(currentPosition, { width, height }, dataPosition) {
+  switch (currentPosition) {
+    case 'top':
+      return {
+        posX: dataPosition.left,
+        posY: dataPosition.top - height,
+      };
+    case 'left':
+      return {
+        posX: dataPosition.left - width,
+        posY: dataPosition.top,
+      };
+    case 'bottom':
+      return {
+        posX: dataPosition.left,
+        posY: dataPosition.bottom,
+      };
+    case 'right':
+      return {
+        posX: dataPosition.left + dataPosition.width,
+        posY: dataPosition.top,
+      };
+    default:
+      break;
+  }
+  return null;
+}
+
+function getProportionsTooltip(link) {
+  const template = createTooltipTemplate({ posX: 0, posY: 0 }, link.title);
+  link.insertAdjacentHTML('afterend', template);
+  const tooltip = document.querySelector('.tooltip');
+  const proportions = {
+    width: tooltip.clientWidth,
+    height: tooltip.clientHeight,
+  };
+  tooltip.remove();
+  return proportions;
+}
+function createTooltipTemplate({ posX, posY }, title) {
   return `
-  <div class="tooltip tooltip_active" style="left: ${left}px; top: ${bottom}px">${title}</div>
+  <div class="tooltip tooltip_active" style="left: ${posX}px; top: ${posY}px">${title}</div>
   `;
 }
