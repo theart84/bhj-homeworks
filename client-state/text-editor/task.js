6c0ed18beee841c4cb9aef2bcbd6929a73884d60
theart84 <artshadow74@gmail.com>
2020-12-18 20:35:44 +0500

    Task 1, subtask completed

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,12 +1,21 @@
 const textareaElement = document.getElementById('editor');
+const clearButtonElement = document.getElementById('clearField');
 
+// Events
 document.addEventListener('DOMContentLoaded', () => loadDataFromLocalStorage());
 textareaElement.addEventListener('input', () => saveDataInLocalStorage());
+clearButtonElement.addEventListener('click', () => clearText());
 
+// Handlers
 function saveDataInLocalStorage() {
   localStorage.setItem('text', textareaElement.value);
 }
 
 function loadDataFromLocalStorage() {
   textareaElement.value = localStorage.getItem('text');
+}
+
+function clearText() {
+  textareaElement.value = '';
+  saveDataInLocalStorage();
 }
